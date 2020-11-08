2485ea7e458cdc64eebf8f47c18ed233dd811a41
JorgeRamirez7 <ramirezradij@spu.edu>
2020-11-07 16:36:20 -0800

    Fix workspace merge conflicts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
-let hostname = '0.0.0.0';
-let port = process.env.PORT || '3000';
+const hostname = 'localhost';
+const port = 3000;
 
 var friends = require("./friends.json"); // Once for all times
 
